c235b49d80400207db170a5e3d6c433fe07bec08
Vinicius Almeida <vinihenriqueh@gmail.com>
2020-05-21 11:01:47 -0300

    Feat: criado MVC
    criado MVC e dado inicio no projeto criando já a home da tela inicial e importando os conteudos

---
+++ application/__init__.py
@@ -0,0 +1,14 @@
+from flask import Flask 
+import os
+
+
+template_folder = os.path.abspath("application/view/templates")
+static_folder = os.path.abspath("application/view/static")
+
+
+app = Flask(__name__, template_folder=template_folder, static_folder=static_folder)
+
+
+@app.route("/")
+def home():
+    return "<h1> hello world </h1>"
