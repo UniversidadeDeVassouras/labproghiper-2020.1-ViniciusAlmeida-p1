1b9121292d562f746f7167531ec429ca51b528f6
Vinicius Almeida <vinihenriqueh@gmail.com>
2020-05-26 15:42:27 -0300

    Feat: Adicionado video teste
    adicionado video para testar as funcionabilidades

--- application/model/dao/videoDAO.py
+++ application/model/dao/videoDAO.py
@@ -1,3 +1,18 @@
 class videoDAO:
    def __init__(self):
-       pass
+       self._list_video = []
+
+    def qtd_max_like (self):
+        qtd_max_like = self._list_video [0]
+        for video in self._list_video:
+            if self._qtd_max_like.get_qtdLike > video.get_qtdLike():
+                qtd_max_like = video
+
+    def add_view (self, video):
+        qtdVisualization += 1
+        video.set_qtdVisualization(qtdVisualization)
+
+    def add_like (self, video):
+        qtdLikes += 1
+        video.set_qtdLike(qtdLikes)
+
