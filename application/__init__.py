d0a32f12c5ab25b023a43ecba4a5d207c94b4fdb
Vinicius Almeida <vinihenriqueh@gmail.com>
2020-05-29 22:12:18 -0300

    Feat: adicionado application
    Adicionado app config, para globalizar algumas rotas

--- application/__init__.py
+++ application/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask 
 import os
+from application.model.dao.categoryDAO import CategoryDAO
+from application.model.dao.videoDAO import videoDAO
+from application.model.dao.commentsDAO import CommentaryDAO
 
 
 template_folder = os.path.abspath("application/view/templates")
@@ -10,6 +13,16 @@ app = Flask(__name__, template_folder=template_folder, static_folder=static_fold
 
 
 
+categories = CategoryDAO ()
+videos = VideoDAO ()
+comments = CommentaryDAO ()
+
+app.config ['categories'] = categories
+app.config ['videos'] = videos
+app.config ['comments'] = comments
+
+
+
 from application.controller import home_controller
 from application.controller import category_controller
-
+from application.controller import video_controller
