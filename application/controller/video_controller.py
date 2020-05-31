88af0d7215d9afdb8cb79b826a60ad5a8ee42e75
Vinicius Almeida <vinihenriqueh@gmail.com>
2020-05-31 18:15:47 -0300

    Fix: modificado video controller
    corrigido rota do video, para poder receber como parametros a quantidade de visualizações e like

--- application/controller/video_controller.py
+++ application/controller/video_controller.py
@@ -1,15 +1,29 @@
 from application import app
-from flask import render_template
+from flask import render_template, request, current_app, jsonify
 from application.model.dao.categoryDAO import CategoryDAO
-from application.model.dao.videoDAO import videoDAO
+from application.model.dao.videoDAO import VideoDAO
 from application.model.entity.comments import Commentary
 
+@app.route ("/video/like", methods = ["POST"])
+def like ():
+    try:
+        videos = current_app.config ['videos']
+        video_id = (request.form["video_id"])
+        video = videos.get_video_by_id(video_id)
+        video.set_qtdLike (video.get_qtdlike() +1)
+        return jsonify(qtd_likes = video.get_qtdlike())
+    except Exception as e:
+        return str(e)
+    
+
+
 @app.route ("/category/<int:category_id>/video/<int:video_id>")
-def video (category_id, video_id)
+def video (category_id, video_id):
     videos = current_app.config ['videos']
     categories = current_app.config ['categories']
-    comments = current_app.config ['comments']
-    category = categories.get_id (category_id)
-    videos_categorie = videos.get_id (video_id)
-    video.set_qtdVisualization(video.get_qtdVisualization() + 1)
-    video_comments = comments.
+    category = categories.search(category_id)
+    video = videos.get_video_by_id(video_id)
+    video.set_qtdVisualization (video.get_qtdVisualization() +1)
+    return render_template ("video.html", video = video)
+
+
