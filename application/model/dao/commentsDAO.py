ca7cc5f3a3b804f59985887790b661ded67957ef
Vinicius Almeida <vinihenriqueh@gmail.com>
2020-06-02 09:45:32 -0300

    Feat: Criado Comentarios
    criado função para comentario no video escolhido

--- application/model/dao/commentsDAO.py
+++ application/model/dao/commentsDAO.py
@@ -1,11 +1,11 @@
 from flask import current_app
 from application.model.entity.comments import Commentary
+from application.model.entity.video import Videos
+from application.model.dao.videoDAO import VideoDAO
 
 class CommentaryDAO:
     def __init__ (self):
         self._comments = []
-        self._comments.append (Commentary("Teste comentario", "Vinicius", 1))
-        self._comments.append (Commentary("Teste comentario 2", "Lindomar", 1))
 
 
     def get_commentary (self):
@@ -16,8 +16,8 @@ class CommentaryDAO:
 
 
     def get_commentary_video (self, video_id):
-        video = current.app_config ['videos']
-        commentary = current.app_config ['comments']
+        video = current_app.config ['videos']
+        commentary = current_app.config ['comments']
         commentary_video = []
         for i, comment in enumerate (commentary.get_commentary()):
             if comment.get_video_commentary_id() == video_id:
--- application/model/entity/comments.py
+++ application/model/entity/comments.py
@@ -1,16 +1,12 @@
 from flask import current_app
 
 class Commentary:
-    def __init__ (self, content, author, video_id):
+    def __init__ (self, content, video_id):
         self._content = content
-        self._author = author
         self._video_id = video_id
 
     def get_content(self):
         return self._content
-
-    def get_author (self):
-        return self._author
 
     def get_video_commentary_id (self):
         return self._video_id
